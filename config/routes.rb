25fd98e70663ff29ebe2cbf8c9a07cfce2ebc3b4
家村健太郎 <aho@iemurakentarounoMacBook-Air.local>
2023-04-28 12:59:04 +0900

    #{コミットメッセージ}

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  # ルートを　app\views\pages\home.html.erb　に設定
+  root 'pages#home'
+  devise_for :users, 
+              path: '', 
+              path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
+              controllers: {registrations: 'registrations'}
+  get 'pages/home'
+  get '/dashboard', to: 'users#dashboard'
+  
+  post '/users/edit', to: 'users#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
